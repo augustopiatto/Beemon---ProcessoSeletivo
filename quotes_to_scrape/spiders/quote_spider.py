ba68fd859fdbfb9875cd9a92538ea5e89631040d
Augusto <ampiatto@hotmail.com>
2023-11-11 13:03:04 -0300

    busca no site todo

--- quotes_to_scrape/spiders/quote_spider.py
+++ quotes_to_scrape/spiders/quote_spider.py
@@ -5,6 +5,18 @@ class QuoteSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com/"]
 
     def parse(self, response):
-        filename = "quote"
-        with open(filename, "wb") as f:
-            f.write(response.body)
+        for quote in response.css("div.quote"):
+            text = quote.css(".text::text").get()
+            author = quote.css("small.author::text").get()
+            tags = quote.css("a.tag::text").getall()
+
+            yield {
+                "text": text,
+                "author": author,
+                "tags": tags,
+            }
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
